e911d6177b001bf5f2cd0527d203e801b2d43530
Veer <rohan.veer1@hp.com>
2020-06-29 18:18:29 +0530

    Added path parameters

--- app/components/MainLayout.jsx
+++ app/components/MainLayout.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import {
   Switch,
-  Route
+  Route,
+  useRouteMatch
 } from 'react-router-dom';
 import { SideBar } from './SideBar';
 import { PageLayout } from './PageLayout';
 
 function MainLayout() {
+  let match = useRouteMatch();
   return (
     <div id='main-wrapper'>
       <SideBar />
       <Switch>
-        <Route path="/view/page-1">
+        <Route path={`${match.path}/page-1`}>
           <PageLayout id={1} />
         </Route>
-        <Route path="/view/page-2">
+        <Route path={`${match.path}/page-2`}>
           <PageLayout id={2} />
         </Route>
-        <Route path="/">
+        <Route path={match.path}>
           <b>TODO</b>
         </Route>
       </Switch>
--- app/components/SideBar.jsx
+++ app/components/SideBar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useRouteMatch } from 'react-router-dom';
 
 function SideBar() {
+  let match = useRouteMatch();
   return (<div>
     <ul>
       <li>
-        <Link to='/view/page-1'>Page 1</Link>
+        <Link to={`${match.url}/page-1`}>Page 1</Link>
       </li>
       <li>
-        <Link to='/view/page-2'>Page 2</Link>
+        <Link to={`${match.url}/page-2`}>Page 2</Link>
       </li>
       <li>
-        <Link to='/'>Home</Link>
+        <Link to={match.isExact ? '/' : match.url}>Back</Link>
       </li>
     </ul>
   </div>);
